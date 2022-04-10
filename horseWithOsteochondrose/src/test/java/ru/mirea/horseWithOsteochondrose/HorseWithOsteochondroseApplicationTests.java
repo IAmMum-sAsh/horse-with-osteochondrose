26291bcef533bafcb04feb34d472cec999dcd309
IAmMum-sAsh <vituchina-natasha11@yandex.ru>
2022-04-10 19:48:10 +0300

    формирование базовой структуры бэкенда проекта и создание функции регистрации

---
+++ horseWithOsteochondrose/src/test/java/ru/mirea/horseWithOsteochondrose/HorseWithOsteochondroseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.horseWithOsteochondrose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorseWithOsteochondroseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
