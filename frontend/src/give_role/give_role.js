53b5527b3de91aecac1599e5f940b2f249b98dd1
IAmMum-sAsh <vituchina-natasha11@yandex.ru>
2022-05-04 13:15:14 +0300

    реализована выдача прав

--- frontend/src/give_role/give_role.js
+++ frontend/src/give_role/give_role.js
@@ -2,19 +2,25 @@ import {withCookies} from "react-cookie";
 import React, { Component } from 'react';
 import Cookies from "universal-cookie";
 import Header from "../header/Header";
+import "./give_role.css"
 
 class GiveRole extends Component {
     constructor(props) {
         super(props);
         this.state = {
             specs: [],
-            spec_id: 0,
+            spec_id: 1,
+            user_id: 0,
             code: props.code ? props.code : '999',
             description: props.description ? props.description : 'Unknown error'
         }
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeID = this.handleChangeID.bind(this);
+        this.handleChangeSP = this.handleChangeSP.bind(this);
         // this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.handleSubmitAdm = this.handleSubmitAdm.bind(this);
+        this.handleSubmitDoc = this.handleSubmitDoc.bind(this);
     }
 
     async getSpecs() {
@@ -23,6 +29,34 @@ class GiveRole extends Component {
 
         return await fetch('/api/specs', {
             method: 'get',
+            headers: new Headers({
+                'Authorization': 'Bearer ' + a,
+                'Content-Type': 'application/json'
+            }),
+        }).then(response => response.json());
+    }
+
+    //give_doctor/{user_id}/{spec_id}
+    async giveDoc() {
+        const cookies = new Cookies();
+        let a = cookies.get('accessToken');
+
+        return await fetch('/api/admin/give_doctor/'+this.state.user_id+'/'+this.state.spec_id, {
+            method: 'put',
+            headers: new Headers({
+                'Authorization': 'Bearer ' + a,
+                'Content-Type': 'application/json'
+            }),
+        }).then(response => response.json());
+    }
+
+    //give_admin/{id}
+    async giveAdm() {
+        const cookies = new Cookies();
+        let a = cookies.get('accessToken');
+
+        return await fetch('/api/admin/give_admin/'+this.state.user_id, {
+            method: 'put',
             headers: new Headers({
                 'Authorization': 'Bearer ' + a,
                 'Content-Type': 'application/json'
@@ -40,7 +74,7 @@ class GiveRole extends Component {
         // const list = this.state.specs.map(specs => <a className="spec-b">{specs.id} - {specs.name}</a>);
         const list = this.state.specs.map(spec => <option value={spec.id}>{spec.name}</option>);
         return (
-            <select value={this.state.spec_id} onChange={this.handleChange}>
+            <select className='input' label="Специальность" value={this.state.spec_id} onChange={this.handleChangeSP}>
                 {list}
             </select>
         );
@@ -48,7 +82,11 @@ class GiveRole extends Component {
 
 
 
-    handleChange(event) {
+    handleChangeID(event) {
+        this.setState({user_id: event.target.value});
+    }
+
+    handleChangeSP(event) {
         this.setState({spec_id: event.target.value});
     }
 
@@ -57,24 +95,85 @@ class GiveRole extends Component {
     //     event.preventDefault();
     // }
 
+    handleSubmitAdm = async e => {
+        e.preventDefault();
+
+        const token = await this.giveAdm();
+        window.location.reload();
+    }
+
+
+    handleSubmitDoc = async e => {
+        e.preventDefault();
+
+        const token = await this.giveDoc();
+        window.location.reload();
+    }
+
     render() {
         return (
             <div className="back-background">
                 <div className='main-important'>
                     <Header />
                     <div className="container">
-                        <h3>Выберите специальность для записи</h3>
-                        {this.renderSpecs()}
+                        <h3>Введите id человека, которому хотите выдать права Доктора</h3>
                     </div>
-
+                    <div className="form" noValidate>
+                        <div className="input-container ic1">
+                            <input
+                                className="input" type="number" min="2" placeholder=" "
+                                id="id"
+                                label="ID"
+                                name="user_id"
+                                onChange={this.handleChangeID}
+                                autoFocus
+                            />
+                            <div className="cut"></div>
+                            <label htmlFor="ID" className="placeholder">ID</label>
+                        </div>
+                        <div className="input-container ic1">
+                            {this.renderSpecs()}
+                        </div>
+                        <button
+                            type="submit"
+                            onClick={this.handleSubmitDoc}
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className="submit"
+                        >
+                            Выдать права
+                        </button>
+                    </div>
+                    <div className="container">
+                        <h3>Введите id человека, которого хотите повысить до Администратора</h3>
+                    </div>
+                    <div className="form" noValidate>
+                        <div className="input-container ic1">
+                            <input
+                                className="input" type="number" min="2" placeholder=" "
+                                id="id"
+                                label="ID"
+                                name="user_id"
+                                onChange={this.handleChangeID}
+                                autoFocus
+                            />
+                            <div className="cut"></div>
+                            <label htmlFor="ID" className="placeholder">ID</label>
+                        </div>
+                        <button
+                            type="submit"
+                            onClick={this.handleSubmitAdm}
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className="submit"
+                        >
+                            Выдать права
+                        </button>
+                    </div>
                 </div>
             </div>
-            // <form onSubmit={this.handleSubmit}>
-            //     <label>
-            //
-            //     </label>
-            //     <input type="submit" value="Submit" />
-            // </form>
         );
     }
 }
